6c8a107d9ba7cb3191b53f21af9d1102fc6eea13
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-06-27 16:39:29 -0700

    Fix bug: call super before using this

--- utils/errors.js
+++ utils/errors.js
@@ -1,6 +1,7 @@
 class ExpressError extends Error {
 
     constructor(message, status) {
+        super()
         this.message = message;
         this.status = status;
     }
@@ -9,14 +10,14 @@ class ExpressError extends Error {
 class BadRequestError extends ExpressError {
      
     constructor(message='Bad request', status=400) {
-        super(message, status)
+        super(message, status);
     }
 }
 
 class NotFoundError extends ExpressError {
      
     constructor(message='Not found', status=404) {
-        super(message, status)
+        super(message, status);
     }
 }
 
