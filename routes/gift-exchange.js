8f3b4abf5d0ae74b75e087bd7d32a7a1db168645
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-06-27 15:58:18 -0700

    Send responses for router

--- routes/gift-exchange.js
+++ routes/gift-exchange.js
@@ -1,16 +1,19 @@
 const express = require('express');
+const GiftExchange = require('../models/gift-exchange');
 const router = express.Router();
 
 router.post('/pairs', (req, res) => {
   const names = req.body.names;
+  const pairs = GiftExchange.pairs(names);
 
-  res.status(200).send("Gotcha")
+  res.status(200).send(pairs);
 })
 
 router.post('/traditional', (req, res) => {
-    const names = req.body.names
+    const names = req.body.names;
+    const traditional_pairings = GiftExchange.traditional(names);
 
-    res.status(200).send("OK")
+    res.status(200).send(traditional_pairings)
 })
 
 
